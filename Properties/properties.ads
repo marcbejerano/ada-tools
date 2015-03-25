06812fc3fcdce12c8b88e650482c4065007a56a6
Marc Bejerano <marc.bejerano@ticketmaster.com>
2015-03-25 11:50:41 -0700

    Updated file headers with License

--- Properties/properties.ads
+++ Properties/properties.ads
@@ -2,8 +2,37 @@
 -- @(#)Last changed:    Mar 18 2015 10:30:00
 -- @(#)Purpose:         Java properties file support
 -- @(#)Author:          Marc Bejerano <marcbejerano@gmail.com>
--- @(#)Copyright:       
--- @(#)Product:         
+-- @(#)Copyright:       Copyright (C) 2015, Marc Bejerano, All Rights Reserved
+-- @(#)Product:         None
+-- @(#)License:         BSD3
+--
+-- Copyright (c) 2015, Marc Bejerano
+-- All rights reserved.
+-- 
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+-- 
+-- * Redistributions of source code must retain the above copyright notice, this
+--   list of conditions and the following disclaimer.
+-- 
+-- * Redistributions in binary form must reproduce the above copyright notice,
+--   this list of conditions and the following disclaimer in the documentation
+--   and/or other materials provided with the distribution.
+-- 
+-- * Neither the name of ada-tools nor the names of its
+--   contributors may be used to endorse or promote products derived from
+--   this software without specific prior written permission.
+-- 
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+-- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+-- DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+-- FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+-- DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+-- SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+-- CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+-- OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+-- OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 with Ada.Containers.Hashed_Maps;    use Ada.Containers;
 with Ada.Containers.Vectors;
@@ -79,8 +108,35 @@ package Properties is
     -- @param input Input file object
     --
     procedure Load(props: in out Properties; input: File_Type);
+
+    --
+    -- Load the properties from the given input file object into
+    -- the Properties object. All of the rules specified by the 
+    -- Java SE 7 Properties class are enforced.<br>
+    -- http://docs.oracle.com/javase/6/docs/api/java/util/Properties.html
+    -- @param props Properties object
+    -- @param filename Name of file to load
+    --
     procedure Load(props: in out Properties; filename: in String);
+
+    --
+    -- Load the properties from the given input file object into
+    -- the Properties object. All of the rules specified by the 
+    -- Java SE 7 Properties class are enforced.<br>
+    -- http://docs.oracle.com/javase/6/docs/api/java/util/Properties.html
+    -- @param props Properties object
+    -- @param input Input file object
+    --
     procedure Load_From_XML(props: in out Properties; input: File_Type);
+
+    --
+    -- Load the properties from the given input file object into
+    -- the Properties object. All of the rules specified by the 
+    -- Java SE 7 Properties class are enforced.<br>
+    -- http://docs.oracle.com/javase/6/docs/api/java/util/Properties.html
+    -- @param props Properties object
+    -- @param filename Name of file to load
+    --
     procedure Load_From_XML(props: in out Properties; filename: in String);
 
     --
